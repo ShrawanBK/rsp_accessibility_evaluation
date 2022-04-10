9cca33510738a2412cff726172da001876dc0715
ShrawanBk <bkshrawanbk@gmail.com>
2022-04-10 23:04:50 +0300

    Add chakra-ui package

--- src/Root/App/index.tsx
+++ src/Root/App/index.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+
+import { ChakraProvider, extendTheme, withDefaultColorScheme } from '@chakra-ui/react';
+
 import ScanForm from '../../components/forms/ScanForm';
 import Sidebar from '../../components/Sidebar';
 import Test from '../../components/Test';
 
 import './styles.css';
 
+const customTheme = extendTheme(withDefaultColorScheme({ colorScheme: 'blue' }));
+
 function App() {
     return (
-        <div className="App">
-            <aside className="side-content">
-                <Sidebar />
-
-            </aside>
-            <main className="main-content">
-                <Routes>
-                    <Route path="/scan_website" element={<ScanForm />} />
-                    <Route path="/saved_scans" element={<Test />} />
-                </Routes>
-            </main>
-        </div>
+        <ChakraProvider theme={customTheme}>
+            <div className="App">
+                <aside className="side-content">
+                    <Sidebar />
+                </aside>
+                <main className="main-content">
+                    <Routes>
+                        <Route path="/scan_website" element={<ScanForm />} />
+                        <Route path="/saved_scans" element={<Test />} />
+                    </Routes>
+                </main>
+            </div>
+        </ChakraProvider>
     );
 }
 
